64d7a00f4784780ae95e250a5d02510a010c2a66
kimgyuha <kimgyuha25@gmail.com>
2023-02-20 23:32:21 +0900

    add :: img require index file
    require and export img files

---
+++ src/assets/index.ts
@@ -0,0 +1,13 @@
+export const Logo = require("./img/logo.png");
+
+/* 프로필 이미지 사진 */
+export const P1 = require("./profileImg/1_default.png");
+export const P2 = require("./profileImg/2_star.png");
+export const P3 = require("./profileImg/3_default.png");
+export const P4 = require("./profileImg/4_default.png");
+export const P5 = require("./profileImg/5_default.png");
+export const P6 = require("./profileImg/6_default.png");
+export const P7 = require("./profileImg/7_default.png");
+export const P8 = require("./profileImg/8_whale.png");
+export const P9 = require("./profileImg/9_tictactoe.png");
+export const P10 = require("./profileImg/10_diamond.png");
