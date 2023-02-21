c558a9f7b2aad5f66f84e05c9af21f863165cbdc
kimgyuha <kimgyuha25@gmail.com>
2023-02-21 23:13:27 +0900

    chore :: bookBackground file
    bookbackground change location

--- src/components/common/Background.tsx
+++ src/components/common/Background.tsx
@@ -1,6 +1,6 @@
 import { ReactNode } from "react";
 import styled from "styled-components";
-import { BookBackground } from "../assets";
+import { BookBackground } from "../../assets";
 
 type PropType = {
   children: ReactNode;
