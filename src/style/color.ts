cfc3c2a75655d09564ee3ddcc4ab6dea02629840
kimgyuha <kimgyuha25@gmail.com>
2023-02-21 11:21:07 +0900

    add :: color style
    add basic, gray, google color

---
+++ src/style/color.ts
@@ -0,0 +1,19 @@
+export const Basic = {
+  black0: "1a1a1a",
+  /** rgba 값 */
+  popupBG: "0,0,0,0.5",
+  RED: "FF0000",
+};
+
+export const Gray = {
+  Elevated0: "F2F2F2",
+  50: "7F7F7F",
+  25: "BFBFBF",
+  10: "E6E6E6",
+};
+
+export const Google = {
+  green: "34A853",
+  Y: "FBBC05",
+  blue: "4285F4",
+};
