6e1fac657696d6b1cb011b7bf518af7932efa0c4
kimgyuha <kimgyuha25@gmail.com>
2023-02-21 11:27:43 +0900

    feat :: header router

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from "./components/common/header";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route/>
+        <Route element={<Header />}>
+          {/* 헤더 적용할 페이지는 여기에  */}
+        </Route>
       </Routes>
     </BrowserRouter>
   );
