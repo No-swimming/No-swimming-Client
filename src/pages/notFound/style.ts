6eb2e6886fa2371546d401359fe629ac8799e763
kimgyuha <kimgyuha25@gmail.com>
2023-02-22 10:02:41 +0900

    add :: 404 Not Found Page

---
+++ src/pages/notFound/style.ts
@@ -0,0 +1,26 @@
+import styled from "styled-components";
+import { Gray } from "../../style/color";
+
+export const Container = styled.div`
+  width: 100vw;
+  height: calc(100vh - 70px);
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 8px;
+  > h1 {
+    font-size: 48px;
+  }
+`;
+
+export const Error404 = styled.p`
+  font-size: 20px;
+  color: ${Gray[50]};
+  font-weight: bold;
+`;
+
+export const ErrorExplain = styled.p`
+  font-size: 20px;
+  text-align: center;
+`;
