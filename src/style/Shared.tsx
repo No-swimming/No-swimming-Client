ca6a694909626e5e53b3d875dc292271a1739eb1
김상구 <spair0039@gmail.com>
2023-02-22 15:06:39 +0900

    add : buttonBlack 속성

--- src/style/Shared.tsx
+++ src/style/Shared.tsx
@@ -1,1 +1,22 @@
 import styled from "styled-components";
+
+export const ButtonBlack = styled.button`
+    color: white;
+    background-color: black;
+    border: none;
+    cursor: pointer;
+
+    font-size: 20px;
+
+    border-radius: 888px;
+    padding: 8px 16px;
+
+    &:hover{
+        background-color: hsl(0, 0%, 10%);
+        transition: 100ms;   
+    }
+    &:active{
+        filter: brightness(75%);
+        transition: 100ms;
+    }
+`
