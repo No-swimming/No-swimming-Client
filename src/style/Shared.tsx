0c3e5ac44116ce2dd4bbe42f37a6e78fdf77f6eb
김상구 <spair0039@gmail.com>
2023-02-22 15:15:11 +0900

    add : 나머지 버튼 스타일 추가

--- src/style/Shared.tsx
+++ src/style/Shared.tsx
@@ -20,3 +20,45 @@ export const ButtonBlack = styled.button`
         transition: 100ms;
     }
 `
+
+export const ButtonBlackTrans = styled.button`
+    color: white;
+    background-color: hsla(0, 0%, 0%, 0.5);
+    border: none;
+    cursor: pointer;
+
+    font-size: 20px;
+
+    border-radius: 888px;
+    padding: 8px 16px;
+
+    &:hover{
+        background-color: hsla(0, 0%, 0%, 0.6);
+        transition: 100ms;   
+    }
+    &:active{
+        filter: brightness(75%);
+        transition: 100ms;
+    }
+`
+
+export const ButtonLight = styled.button`
+    color: black;
+    background-color: hsla(0, 0%, 95%, 1);
+    border: none;
+    cursor: pointer;
+
+    font-size: 20px;
+
+    border-radius: 888px;
+    padding: 8px 16px;
+
+    &:hover{
+        background-color: hsla(0, 0%, 85%, 1);
+        transition: 100ms;   
+    }
+    &:active{
+        filter: brightness(75%);
+        transition: 100ms;
+    }
+`
