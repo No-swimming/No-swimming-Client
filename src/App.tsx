15a65952e236f472156070d6d601bff762e6acec
kimgyuha <kimgyuha25@gmail.com>
2023-02-27 16:45:39 +0900

    fix :: main Page router

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "./components/common/header";
+import MainPage from "./pages/main";
+import NotFound from "./pages/notFound";
 
 function App() {
   return (
@@ -7,6 +9,8 @@ function App() {
       <Routes>
         <Route element={<Header />}>
           {/* 헤더 적용할 페이지는 여기에  */}
+          <Route path="*" element={<NotFound />} />
+          <Route path="/" element={<MainPage />} />
         </Route>
       </Routes>
     </BrowserRouter>
