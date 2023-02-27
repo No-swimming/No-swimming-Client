e6d2c20dbb35ca1d0a737648c7a6bebdc59cdc5d
kimgyuha <kimgyuha25@gmail.com>
2023-02-27 16:45:27 +0900

    add :: searchBar, bestBook component
    use MainPage

---
+++ src/components/pages/main/bestBook.tsx
@@ -0,0 +1,94 @@
+import styled from "styled-components";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { Gray } from "../../../style/color";
+
+const BESTBOOK_KEY = process.env.REACT_APP_BESTBOOK_KEY;
+const corsUrl = "http://monotype.iptime.org:10888/";
+const bookUrl = `http://apis.data.go.kr/6260000/BookSearchWordBestService/getBookSearchWordBest?serviceKey=${BESTBOOK_KEY}`
+const axiosBook = {
+  method: 'get',
+  url: `${corsUrl}${bookUrl}`,
+  params: {
+    numOfRows: 10,
+    pageNo: 1,
+    resultType: 'json'
+  }
+}
+
+type bookType = { search_word: string, rank: string, type: string }
+
+const BestBook = (): JSX.Element => {
+  const navigate = useNavigate();
+  useEffect(() => {
+    if (!sessionStorage.getItem("book1")) {
+      axios(axiosBook)
+        .then(res => {
+          const list = res.data.getBookSearchWordBest.body.items.item;
+          sessionStorage.setItem("book1", list[9].search_word);
+          sessionStorage.setItem("book2", list[8].search_word);
+          sessionStorage.setItem("book3", list[7].search_word);
+        })
+        .catch(err => {
+          console.log(err)
+        })
+    }
+  }, [])
+
+
+  return (
+    <Container>
+      <div>
+        <Title>인기 도서</Title>
+        <Line>|</Line>
+        <Books>
+          <p onClick={() => navigate(`/search/${sessionStorage.getItem("book1")}`)}>{sessionStorage.getItem("book1")}</p>
+          <p onClick={() => navigate(`/search/${sessionStorage.getItem("book2")}`)}>{sessionStorage.getItem("book2")}</p>
+          <p onClick={() => navigate(`/search/${sessionStorage.getItem("book3")}`)}>{sessionStorage.getItem("book3")}</p>
+        </Books>
+      </div>
+      <Plus>+ 더보기</Plus>
+    </Container >
+  )
+}
+
+export default BestBook;
+
+const Container = styled.div`
+  display: flex;
+  justify-content: space-between;
+  width: 768px;
+  font-size: 20px;
+  color: ${Gray[50]};
+  margin-top: 12px;
+  margin-bottom: 15vh;
+  >div{
+    display: flex;
+    gap: 12px;
+    width: 688px;
+    height: 26.4px;
+  }
+`
+const Title = styled.p`
+  font-weight: bold;
+  width: 75.5px;
+  white-space : nowrap;
+`
+const Line = styled.p`
+  color: ${Gray[25]};
+`
+const Books = styled.div`
+  display: flex;
+  gap: 12px;
+  >p{
+    cursor: pointer;
+  }
+`
+const Plus = styled.button`
+  background-color: rgba(0,0,0,0);
+  border: none;
+  font-size: 20px;
+  color:${Gray[50]};
+  cursor: pointer;
+`
