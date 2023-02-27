a1794574b837fadbc56afdc762e9d1ff4330d6b3
김상구 <spair0039@gmail.com>
2023-02-27 17:21:43 +0900

    add : 비밀번호시 eye 구현

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,3 +1,5 @@
+import { Eye24Regular, EyeOff24Regular } from "@fluentui/react-icons";
+import { useState } from "react";
 import { MainInputContainer } from "./style";
 
 type propType = {
@@ -7,13 +9,35 @@ type propType = {
     onChange?: ()=>void;
 }
 
+type passwordEyeType = {
+    click: ()=>void;
+    eyeOn?: boolean;
+}
+
 function Input({placeholder, value, isPassword, onChange}:propType){
+    const [isVisable, setVisable] = useState(false);
+    function pwdVisableSwitch(){
+        setVisable(!isVisable);
+    }
 
     return(
             <MainInputContainer >
                 {placeholder}
-                <input value={value} type={isPassword ? "password" : "text"} onChange={onChange} />
+                <div>
+                    <input value={value} type={isPassword && isVisable===false ? "password" : "text"} onChange={onChange} />
+                    {isPassword ? <PasswordEye click={pwdVisableSwitch} eyeOn={isVisable}/> : null}
+                </div>
             </MainInputContainer>
+    )
+}
+
+function PasswordEye({eyeOn, click}:passwordEyeType){
+    return(
+        <>
+            <button onClick={click}>
+                {eyeOn ? <EyeOff24Regular /> : <Eye24Regular />}
+            </button>
+        </>
     )
 }
 
