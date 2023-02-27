c4164c4bae3129d5cf2d0be2aaf5152dda636ef8
김상구 <spair0039@gmail.com>
2023-02-27 16:08:00 +0900

    add : Input 디자인

---
+++ src/components/Input/Input.tsx
@@ -0,0 +1,13 @@
+function Input(){
+    return(
+        <div>
+            <label >
+                abc
+                <input/>
+            </label>
+            
+        </div>
+    )
+}
+
+export default Input;
