b9f1ff42bfe748fcec27b5f675e715de1c948bdf
kimgyuha <kimgyuha25@gmail.com>
2023-02-27 16:44:49 +0900

    add :: main Page
    index.tsx style.ts

---
+++ src/pages/main/style.ts
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const Logo = styled.img`
+  width: 193px;
+  margin-top: 15vh;
+  margin-bottom: 20vh;
+`;
+
+export const Search = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`
