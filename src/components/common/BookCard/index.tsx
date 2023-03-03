60ae970b827f2961bd05305faec01e317026d86d
김상구 <spair0039@gmail.com>
2023-03-03 18:15:07 +0900

    add: LEGACY로부터 복제

---
+++ src/components/common/BookCard/index.tsx
@@ -0,0 +1,121 @@
+import { Heart24Filled, Heart24Regular } from '@fluentui/react-icons';
+import axios from 'axios';
+import { average } from 'color.js';
+import { useState } from 'react';
+import * as _ from './style';
+
+type cardType = {
+    hearted?: boolean;
+    data: bookData;
+}
+
+type bookData = {
+    title: string;
+    image: string;
+    link: string;
+    author: string;
+    pubdate: string;
+    isbn: string;
+    description: string;
+}
+
+type cardHeartType = {
+    onClick: any;
+    hearted?: boolean;
+}
+
+function CardHeart({onClick, hearted}:cardHeartType){
+    return(
+        <_.ButtonIcon onClick={onClick}>
+            {hearted ? <Heart24Filled primaryFill="#ff0000" /> : <Heart24Regular primaryFill="#ffffff" />}
+        </_.ButtonIcon>
+    );
+};
+
+function CardLarge({hearted, data}:cardType){
+    const [bgcolor,setBgcolor] = useState("#000000");
+
+    function getAverage(){
+        average(`http://monotype.iptime.org:10888/${data.image}`,
+            {format:"hex"}
+        )
+        .then(color => setBgcolor(color as string));
+    };
+
+    return(
+        <_.CardBg backgroud={bgcolor}>
+            <img src={data.image} onLoad={getAverage} />
+            <div>
+                <_.CardTitle>{data.title}</_.CardTitle>
+                <_.CardTitle className='trans'>{data.author} | {data.pubdate.substr(0,4)}</_.CardTitle>
+                <_.CardBody>{data.description.substr(0,65)+"..."}</_.CardBody>
+                <div>
+                    <_.ButtonBlack className='icon'>
+                        <CardHeart onClick="afsd" hearted={hearted}/>
+                    </_.ButtonBlack>
+                    <_.ButtonBlack>
+                        읽은 책에 추가
+                    </_.ButtonBlack>
+                    <_.ButtonBlack>
+                        독서록 작성
+                    </_.ButtonBlack>
+                </div>
+            </div>
+        </_.CardBg>
+    )
+}
+
+function CardMini({data}:cardType){
+    const [bgcolor,setBgcolor] = useState("#000000");
+
+    function getAverage(){
+        average(`http://monotype.iptime.org:10888/${data.image}`,
+            {format:"hex"}
+        )
+        .then(color => setBgcolor(color as string));
+    };
+
+    return(
+        <_.CardBgMini background={bgcolor}>
+            <img src={data.image} onLoad={getAverage} />
+            <div>
+                <_.CardTitleMini>{data.title}</_.CardTitleMini>
+                <_.CardTitleMini className='trans'>{data.author} | {data.pubdate.substring(0,4)}</_.CardTitleMini>
+            </div>
+        </_.CardBgMini>
+    )
+}
+
+function BookInfoModal({hearted, data}:cardType){
+    function closeModal(){
+        console.log("modalclosed");
+    }
+
+    return(
+        <_.InfoTransBlack onClick={closeModal}>
+            <_.InfoCardBG>
+                <img src={data.image}/>
+                <_.InfoCardInnerContainer>
+                    <_.CardTitle>{data.title}</_.CardTitle>
+                    <_.CardTitleMini className='trans'>{data.author} | {data.pubdate.substr(0,4)}</_.CardTitleMini>
+                    <span className='row'>
+                        <_.CardTitleMini className='trans'>ISBN</_.CardTitleMini>
+                        <_.CardTitleMini className='trans lite'>{data.isbn}</_.CardTitleMini>
+                    </span>
+                    <span>
+                        <_.CardTitleMini className='trans'>설명</_.CardTitleMini>
+                        <_.CardTitleMini className='trans lite'>{data.description}</_.CardTitleMini>
+                    </span>
+                    <div>
+                        <_.ButtonBlack>(하트)</_.ButtonBlack>
+                        <_.ButtonBlack>읽음으로 표시</_.ButtonBlack>
+                        <_.ButtonBlack>독서록 작성</_.ButtonBlack>
+                        <_.ButtonBlack>네이버 도서에서 보기</_.ButtonBlack>
+                    </div>
+                </_.InfoCardInnerContainer>
+            </_.InfoCardBG>
+        </_.InfoTransBlack>
+    )
+}
+
+export {CardLarge, CardMini, BookInfoModal};
---
+++ src/components/common/BookCard/style.ts
@@ -0,0 +1,138 @@
+import styled from "styled-components";
+
+export const CardBg = styled.div<{backgroud:string}>`
+    display:flex;
+    gap: 12px;
+    padding: 12px;
+    box-sizing: border-box;
+    border-radius: 12px;
+    background: ${props => props.backgroud};
+    color: #FFFFFF;
+    width: 650px;
+    height: 200px;
+    & > div > div{
+        display:flex;
+        gap: 8px;
+        align-items: center;
+    }
+    & > div{
+        display: flex;
+        flex-direction: column;
+        gap: 4px;
+    }
+    & > img{
+        width: 124px;
+        border-radius:4px;
+    }
+`;
+
+export const CardBgMini = styled.div<{background:string}>`
+    display: flex;
+    align-items: center;
+    border-radius: 8px;
+    width: 438px;
+    height: 80px;
+    padding: 4px;
+    gap: 8px;
+    background: ${props => props.background};
+    color: white;
+    font-weight: 700;
+    & > img{
+        height: inherit;
+        border-radius: 4px;
+    }
+`
+
+export const CardTitle = styled.p`
+    font-weight: 700;
+    font-size: 24px;
+    &.trans{
+        color: #0000007f;
+    }
+`;
+
+export const CardTitleMini = styled.p`
+    font-weight: 700;
+    font-size: 20px;
+    &.trans{
+        color: #0000007f;
+    }
+    &.lite{
+        font-weight: 400;
+    }
+    &.desc{
+        height: 12ex;
+        overflow: scroll;
+    }
+`;
+
+export const CardBody = styled.p`
+    font-weight: 400;
+    font-size: 20px;
+    opacity: 0.5;
+`;
+
+export const ButtonBlack = styled.button`
+    display:flex;
+
+    padding: 8px 16px;
+    background: #0000007f;
+    border-radius: 99px;
+    font-size: 20px;
+    color: #FFFFFF;
+    border: none;
+    &.icon{
+        padding: 8px 8px;
+    }
+`;
+
+export const ButtonIcon = styled.button`
+    background: none;
+    border: none;
+    cursor: pointer;
+`
+
+export const InfoTransBlack = styled.div`
+    width: 100vw;
+    height:100vh;
+    z-index: 888;
+    position: absolute;
+    background-color:#0000007F;
+`
+
+export const InfoCardBG = styled.div`
+    z-index: 889;
+    position: absolute;
+    top:50%;
+    left: 50%;
+    transform: translate(-50%,-50%);
+    background-color: #ffffff;
+
+    padding: 16px;
+    box-sizing: border-box;
+    border-radius: 16px;
+
+    display: flex;
+    flex-direction: column;
+
+
+    & > img{
+        width: 124px;
+        border-radius: 4px;
+    }
+`
+
+export const InfoCardInnerContainer = styled.div`
+    & > span.row{
+        display: flex;
+        gap: 4px;
+    }
+    & > div{
+        display: flex;
+        gap: 8px;
+    }
+`
+
+export const ButtonHorizionalLister = styled.div`
+
+`
