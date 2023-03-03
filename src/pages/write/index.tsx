bb7845e87b3236bb115b2309fe39b170288fe0d9
kimgyuha <kimgyuha25@gmail.com>
2023-03-03 19:03:13 +0900

    fix :: write page
    finish UI

--- src/pages/write/index.tsx
+++ src/pages/write/index.tsx
@@ -1,11 +1,34 @@
-import BookTeacher from "../../components/pages/write/bookTeacher";
+import BookTeacher from "../../components/write/bookTeacher";
+import BookWrite from "../../components/write/bookWrite";
 import * as _ from "./style";
+import { useState } from "react";
+import Submit from "../../components/write/submit";
 
+type valueType = {
+  teacher_id: number;
+  title: string;
+  content: string;
+};
 const Write = (): JSX.Element => {
+  const [value, setValue] = useState<valueType>({
+    teacher_id: 0,
+    title: "",
+    content: "",
+  });
+
+  const handleInputChange = (props: string) => (event: any) => {
+    setValue({ ...value, [props]: event.target.value });
+  };
+
+  const onClickSubmit = () => { };
+  const onClickNoSubmit = () => { };
+
   return (
     <_.Container>
       <h1>독서록 작성</h1>
       <BookTeacher />
+      <BookWrite value={value} handleInputChange={handleInputChange} setValue={setValue} />
+      <Submit />
     </_.Container>
   )
 }
--- src/pages/write/style.ts
+++ src/pages/write/style.ts
@@ -1,7 +1,8 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-  width: 100vw;
   display: flex;
   flex-direction: column;
-`
+  width: 100vw;
+  padding: 70px 13% 70px 13%;
+`;
