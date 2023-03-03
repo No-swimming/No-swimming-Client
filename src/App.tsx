a3c15d180b65ee58deaad2044ece73692ec4af97
kimgyuha <kimgyuha25@gmail.com>
2023-03-03 19:04:35 +0900

    fix :: write page router

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "./components/common/header";
 import MainPage from "./pages/main";
 import NotFound from "./pages/notFound";
+import Write from "./pages/write";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
           {/* 헤더 적용할 페이지는 여기에  */}
           <Route path="*" element={<NotFound />} />
           <Route path="/" element={<MainPage />} />
+          <Route path="/write/:isbn" element={<Write />} />
         </Route>
       </Routes>
     </BrowserRouter>
