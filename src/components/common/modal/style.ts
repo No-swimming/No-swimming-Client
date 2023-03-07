9f9372366adfb9e384b476738ced6f6c494ce75d
kimgyuha <kimgyuha25@gmail.com>
2023-03-07 20:08:18 +0900

    feat :: modal component
    available within components
    scroll protection

--- src/components/common/modal/style.ts
+++ src/components/common/modal/style.ts
@@ -8,12 +8,20 @@ export const Container = styled.div`
   justify-content: center;
   align-items: center;
   position: fixed;
+  top:50%;
+  left:50%;
+  transform: translate(-50%, -44%);
 `;
 
 export const ChildrenContainer = styled.div<{
   width?: string;
   height?: string;
   padding?: string;
+  display?: string;
+  justifyContent?: string;
+  alignItems?: string;
+  gap?: string;
+  flexDirection?: string;
 }>`
   background-color: white;
   width: ${(props) => props.width ?? "466px"};
@@ -21,6 +29,10 @@ export const ChildrenContainer = styled.div<{
   border-radius: 8px;
   z-index: 10000;
   padding: ${(props) => props.padding ?? "40px 39px"};
+  display: ${(props) => props.display ?? ""};
+  justify-content: ${(props) => props.justifyContent ?? ""};
+  align-items: ${(props) => props.alignItems ?? ""};
+  flex-direction: ${(props) => props.flexDirection ?? ""};
 `;
 
 export const BackDrop = styled.div`
@@ -28,6 +40,7 @@ export const BackDrop = styled.div`
   height: 100%;
   position: fixed;
   top: 0;
+  left: 0;
   z-index: 9999;
   background-color: rgba(${Transparent.Black50});
 `;
