9e1486fb1bab631d9c4706c059a1d47a608dc272
kimgyuha <kimgyuha25@gmail.com>
2023-03-20 09:11:01 +0900

    feat :: modal style 수정

--- src/components/common/modal/index.tsx
+++ src/components/common/modal/index.tsx
@@ -1,4 +1,4 @@
-import { ReactNode } from "react";
+import { ReactNode, useEffect } from "react";
 import * as _ from "./style";
 
 type PropsType = {
@@ -8,16 +8,30 @@ type PropsType = {
   height?: string;
   padding?: string;
   display?: string;
+  alignItems?: string;
   justifyContent?: string;
   gap?: string;
+  flexDirection?: string;
 }
 
-const Modal = ({ children, onClickToggleModal, width, height, padding, display, justifyContent, gap }: PropsType) => {
+const Modal = ({ children, onClickToggleModal, width, height, padding, display, justifyContent, gap, flexDirection, alignItems }: PropsType) => {
 
-
+  useEffect(() => {
+    document.body.style.cssText = `
+      position: fixed; 
+      top: -${window.scrollY}px;
+      overflow-y: scroll;
+      width: 100%;`;
+    return () => {
+      const scrollY = document.body.style.top;
+      document.body.style.cssText = "";
+      window.scrollTo(0, parseInt(scrollY || "0", 10) * -1);
+    };
+  }, []);
+  
   return (
     <_.Container>
-      <_.ChildrenContainer width={width} height={height} padding={padding} display={display} justifyContent={justifyContent} gap={gap}>
+      <_.ChildrenContainer width={width} height={height} padding={padding} display={display} justifyContent={justifyContent} gap={gap} flexDirection={flexDirection} alignItems={alignItems}>
         {children}
       </_.ChildrenContainer>
       <_.BackDrop onClick={(e: React.MouseEvent) => {
@@ -34,6 +48,7 @@ export default Modal;
 /**
  * 모달 사용법
  * 시용하려는 페이지에 적용
+ * useCallback은 많이 사용하는 모달에서만 사용하세요. 오히려 최적화가 아니라 속도가 느려집니다.
   const [isOpenModal, setIsOpenModal] = useState<boolean>(false);
   const onClickToggleModal = useCallback(() => {
     setIsOpenModal(!isOpenModal);
