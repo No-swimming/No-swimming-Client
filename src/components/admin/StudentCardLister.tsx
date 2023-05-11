51d45c568882fbd03522b45d2cc81091ca7de0ae
김상구 <spair0039@gmail.com>
2023-05-11 20:08:56 +0900

    add: CardLister

---
+++ src/components/admin/StudentCardLister.tsx
@@ -0,0 +1,20 @@
+import StudentCard from "./studentCard"
+
+const StudentCardLister = () => {
+    return(
+        <div>
+            <StudentCard data="1"/>
+            <StudentCard data="1"/>
+            <StudentCard data="1"/>
+            <StudentCard data="1"/>
+            <StudentCard data="1"/>
+            <StudentCard data="1"/>
+            <StudentCard data="1"/>
+            <StudentCard data="1"/>
+            <StudentCard data="1"/>
+            <StudentCard data="1"/>
+        </div>
+    )
+}
+
+export default StudentCardLister;
