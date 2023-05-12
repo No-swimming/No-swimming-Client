a58a01fb5c6fcec90c5b3add01bf80f009736b85
김상구 <spair0039@gmail.com>
2023-05-12 19:26:53 +0900

    add: typing

--- src/components/admin/studentCard.tsx
+++ src/components/admin/studentCard.tsx
@@ -2,17 +2,13 @@ import styled from "styled-components";
 import { Gray } from "../../style/color";
 import { Basic } from "../../style/color";
 
-type studentPropsType = {
-    data: string;
-}
-
 const StudentCard = ({data}:studentPropsType) => {
     return(
         <StudentContainer>
             <ProfileImg src="https://i.guim.co.uk/img/media/e3d9827f235ac40064f15d7df25024aec60500cb/0_134_5616_3370/master/5616.jpg?width=1200&height=1200&quality=85&auto=format&fit=crop&s=56f9da8e992f2558c4709614daf82a69" />
             <Vcontainer>
-                <p>최양성명</p>
-                <p>8학년 8반 88번</p>
+                <p>{data.name}</p>
+                <p>{data.grade}학년 {data.class_num}반 {data.number}번</p>
             </Vcontainer>
         </StudentContainer>
     )
