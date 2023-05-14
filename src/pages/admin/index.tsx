cd6fa713aa02bce97debec2e24165b1c91e59e3a
김상구 <spair0039@gmail.com>
2023-05-14 15:02:02 +0900

    add: server intigration code

--- src/pages/admin/index.tsx
+++ src/pages/admin/index.tsx
@@ -1,7 +1,37 @@
+import { useEffect, useState } from "react";
+import StudentCardLister from "../../components/admin/StudentCardLister";
+import axios from "axios";
+
 const Admin = ():JSX.Element => {
+    const [Data,setData] = useState<studentArrayType>(
+        {
+            student_list:[]
+        }
+    );
+    const [GradeFilter,setGradeFilter] = useState<number>(0);
+    const [ClassFilter, setClassFilter] = useState<number>(0);
+
+    useEffect(()=>{
+        axios({
+            method: 'GET',
+            url:`${process.env.REACT_APP_MONO}${process.env.REACT_APP_BASEURL}student/list`,
+            params:{
+                grade: GradeFilter===0 ? null : GradeFilter,
+                classNum: ClassFilter===0 ? null : ClassFilter 
+            }
+        })
+        .then((res)=>{
+            setData(res.data as studentArrayType);
+            console.log(res)
+        })
+        .catch((error)=>{
+            console.log(error);
+        })
+    },[ClassFilter, GradeFilter]);
+
     return(
         <>
-            halo
+            <StudentCardLister student_list={Data.student_list}/>
         </>
     )
 }
