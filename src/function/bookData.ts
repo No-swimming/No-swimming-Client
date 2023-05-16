551b6c9931bd86e768022b79129829a4a4e5859c
kimgyuha <kimgyuha25@gmail.com>
2023-05-16 09:30:46 +0900

    add :: isbn, bookData 받기 함수

--- src/function/bookData.ts
+++ src/function/bookData.ts
@@ -2,20 +2,31 @@ import axios from "axios";
 
 const NAVER_ID = process.env.REACT_APP_NAVER_ID;
 const NAVER_SECRET_KEY = process.env.REACT_APP_NAVER_SECRET_KEY;
-const corsUrl = "http://monotype.iptime.org:10888/";
-const openNaverUrl =
-  "https://openapi.naver.com/v1/search/book_adv.json?d_isbn=";
-{
-}
+const corsUrl = process.env.REACT_APP_MONO;
+const openNaverUrl = `${process.env.REACT_APP_SEARCH_BOOK}_adv.json?d_isbn=`;
+
 /** 책 정보 가져오는 함수
  * 무조건 isbn으로 보내줘야함!!
  * 숫자 배열 -> json배열
  */
+
+type BookType = {
+  title: string;
+  link: string;
+  image: string;
+  author: string;
+  discount: string;
+  publisher: string;
+  pubdate: string;
+  isbn: string;
+  description: string;
+};
+
 export const BookDataArr = (number: number[]) => {
-  const book: string[] = [];
+  const book: BookType[] = [];
   number.map((isbn) => {
     if (localStorage.getItem(String(isbn))) {
-      book.push(JSON.parse(localStorage.getItem(String(isbn)) || "{}"));
+      book.push(JSON.parse(localStorage.getItem(String(isbn))!));
     } else {
       const axiosBook = {
         method: "get",
