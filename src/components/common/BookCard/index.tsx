2b9891558bf38ed1c75f1fcd934fde34baf8c2cd
kimgyuha <kimgyuha25@gmail.com>
2023-05-19 23:04:01 +0900

    fix :: bookcard ...

--- src/components/common/BookCard/index.tsx
+++ src/components/common/BookCard/index.tsx
@@ -5,6 +5,7 @@ import * as _ from './style';
 import { Button } from '../../../style/Button';
 import Modal from '../modal';
 import tinycolor from "tinycolor2";
+import { useNavigate } from 'react-router-dom';
 
 type cardType = {
     data: bookData;
@@ -20,25 +21,27 @@ type bookData = {
     description: string;
 }
 
-function CardLarge({data}:cardType){
-    const [bgcolor,setBgcolor] = useState<string>("#000000");
+function CardLarge({ data }: cardType) {
+    const [bgcolor, setBgcolor] = useState<string>("#000000");
     const [textColor, setTextColor] = useState<string>("#ffffff");
     const [isOpenModal, setIsOpenModal] = useState<boolean>(false);
     const onClickToggleModal = useCallback(() => {
         setIsOpenModal(!isOpenModal);
     }, [isOpenModal]);
 
-    useEffect(()=>{
-        setTextColor(tinycolor(bgcolor).isLight() ? 'black' : 'white' );
-    },[bgcolor]);
+    const navigate = useNavigate();
 
-    function getprominent(){
+    useEffect(() => {
+        setTextColor(tinycolor(bgcolor).isLight() ? 'black' : 'white');
+    }, [bgcolor]);
+
+    function getprominent() {
         prominent(`${process.env.REACT_APP_MONO}${data.image}`,
-            {format:"hex", amount:1}
+            { format: "hex", amount: 1 }
         )
-        .then(color => setBgcolor(color as string));
+            .then(color => setBgcolor(color as string));
     };
-    return(
+    return (
         <>
             {isOpenModal &&
                 <Modal onClickToggleModal={onClickToggleModal}>
@@ -46,10 +49,10 @@ function CardLarge({data}:cardType){
                 </Modal>
             }
             <_.CardBg backgroud={bgcolor} text={textColor}>
-                <img src={data.image} onLoad={getprominent} onClick={()=>setIsOpenModal(!isOpenModal)} alt='책 표지' />
+                <img src={data.image} onLoad={getprominent} onClick={() => setIsOpenModal(!isOpenModal)} alt='책 표지' />
                 <div>
                     <_.CardTitle>{data.title}</_.CardTitle>
-                    <_.CardTitle className='trans'>{data.author} | {data.pubdate.substring(0,4)}</_.CardTitle>
+                    <_.CardTitle className='trans'>{data.author} | {data.pubdate.substring(0, 4)}</_.CardTitle>
                     <_.CardBody>{data.description}</_.CardBody>
                     <div>
                         <Button BlackTrans className='icon'>
@@ -58,7 +61,7 @@ function CardLarge({data}:cardType){
                         <Button BlackTrans>
                             읽은 책에 추가
                         </Button>
-                        <Button BlackTrans>
+                        <Button BlackTrans onClick={() => navigate(`/write/${data.isbn}`)}>
                             독서록 작성
                         </Button>
                     </div>
@@ -68,31 +71,31 @@ function CardLarge({data}:cardType){
     )
 }
 
-function CardMini({data}:cardType){
-    const [bgcolor,setBgcolor] = useState("#000000");
+function CardMini({ data }: cardType) {
+    const [bgcolor, setBgcolor] = useState("#000000");
 
-    function getprominent(){
+    function getprominent() {
         prominent(`http://monotype.iptime.org:10888/${data.image}`,
-            {format:"hex"}
+            { format: "hex" }
         )
-        .then(color => setBgcolor(color as string));
+            .then(color => setBgcolor(color as string));
     };
 
-    return(
+    return (
         <_.CardBgMini background={bgcolor}>
             <img src={data.image} onLoad={getprominent} />
             <div>
                 <_.CardTitleMini>{data.title}</_.CardTitleMini>
-                <_.CardTitleMini className='trans'>{data.author} | {data.pubdate.substring(0,4)}</_.CardTitleMini>
+                <_.CardTitleMini className='trans'>{data.author} | {data.pubdate.substring(0, 4)}</_.CardTitleMini>
             </div>
         </_.CardBgMini>
     )
 }
 
-function CardModal({data}:cardType){
-    return(
-        <_.BookCoverImg src={data.image}/>
+function CardModal({ data }: cardType) {
+    return (
+        <_.BookCoverImg src={data.image} />
     )
 }
 
-export {CardLarge, CardMini};
+export { CardLarge, CardMini };
