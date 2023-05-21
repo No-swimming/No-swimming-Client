5d9a1c8b88b00b626a47b89d0fdd5ebeeee79fcb
김상구 <spair0039@gmail.com>
2023-05-21 23:15:16 +0900

    style: styling radio

--- src/components/common/Radio.tsx
+++ src/components/common/Radio.tsx
@@ -1,4 +1,7 @@
 import { SetStateAction } from "react";
+import styled from "styled-components";
+import { Gray } from "../../style/color";
+import { Basic } from "../../style/color";
 
 interface selectorPropsType{
     title: string;
@@ -15,12 +18,38 @@ const Radio = ({title, countData, realData, state, setState, id}:selectorPropsTy
     }
 
     return(
-        <label htmlFor={id}>
-            <input type="radio" value={title} checked={state===realData} id={id} onChange={onChangeHandler}/>
+        <Label htmlFor={id}>
+            <NoAppearance type="radio" value={title} checked={state===realData} id={id} onChange={onChangeHandler}/>
             {title}
-            <p>{state===realData ? countData : null}</p>
-        </label>
+            {state===realData ? <GraySpan>{countData}</GraySpan> : null}
+        </Label>
     )
 }
 
 export default Radio;
+
+const NoAppearance = styled.input`
+    appearance: none;
+    display: none;
+`
+
+const Label = styled.label`
+    font-weight: 700;
+    font-size: 20px;
+
+    display: flex;
+    flex-direction: row;
+    align-items: flex-start;
+    padding: 6px 12px;
+    gap: 8px;
+
+    outline: 2px solid ${Basic.black0};
+    border-radius: 9888px;
+
+    background: ${Gray.Elevated0};
+`
+
+const GraySpan = styled.span`
+    font-weight: 400;
+    color: ${Gray[50]};
+`
