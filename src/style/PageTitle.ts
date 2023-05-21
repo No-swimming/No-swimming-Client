f5285857e2bc04c133d947a357a74b5c4634d592
김상구 <spair0039@gmail.com>
2023-05-21 23:25:24 +0900

    style: add pagetitle style

---
+++ src/style/PageTitle.ts
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+import { Basic } from "./color";
+
+const PageTitle = styled.p`
+    font-weight: 700;
+    font-size: 32px;
+    color: ${Basic.black0};
+`
+
+export {PageTitle};
